019326048b5c55e13b2ab1281f8879fe6b3e3534
Qamarul-Fattah <qamarulfattah@gmail.com>
2023-03-31 15:55:48 +0800

    Hello World
    Testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,8 @@
-import java.util.Random;
-import java.util.ArrayList;
-import java.util.List;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<String>();
-        list.add("Celia");
-        list.add("Le Yi");
-        list.add("Grace");
-        list.add("Jena");
-        list.add("Lina");
-
-        Random randomizer = new Random();
-        String random = list.get(randomizer.nextInt(list.size()));
-
-        String name = random;
+        String name = "Hello World!";
         printSomething(name);
         // System.out.println("Hello, World!");
 
